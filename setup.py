474c729839d94b4348e6d632d35d0aa41ee4fcc4
Fish <jonathan.fisher@nbrown.co.uk>
2020-01-06 13:38:33 +0000

    desc

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='Statisk',
-    version='0.0.11',
+    version='0.0.12',
     packages=['statisk'],
     url='https://github.com/fiskurgit/PyStatisk',
     license='GNU General Public License v3.0',
     author='fiskurgit',
     author_email='fiskdebug@gmail.com',
     description='Simple markdown based low bandwidth static site generator',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires='>=3', install_requires=['markdown', 'termcolor', 'Pillow'],
     entry_points={
         'console_scripts': [
